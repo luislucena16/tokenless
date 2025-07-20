d2e0a0a08fd2ea624a0f95d3e1266d7b23a64bf7
luislucena16 <lucenaluis137@gmail.com>
2025-07-21 00:25:06 +0200

    feat: add launcher configs for standalone and testnet

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,7 +1,6 @@
-
 import path from 'node:path';
 import { NetworkId, setNetworkId } from '@midnight-ntwrk/midnight-js-network-id';
-import { QuickStarterPrivateStateId } from '@quick-starter/quick-starter-api';
+import { TokenlessPrivateStateId } from '@tokenless/tokenless-api';
 
 export interface Config {
   readonly privateStateStoreName: string;
@@ -18,9 +17,9 @@ export interface Config {
 export const currentDir = path.resolve(new URL(import.meta.url).pathname, '..');
 
 export class StandaloneConfig implements Config {
-  privateStateStoreName = QuickStarterPrivateStateId
+  privateStateStoreName = TokenlessPrivateStateId
   logDir = path.resolve(currentDir, '..', 'logs', 'standalone', `${new Date().toISOString()}.log`);
-    zkConfigPath = path.resolve(currentDir, '..', '..', 'contract', 'dist', 'managed', 'quick-starter');
+    zkConfigPath = path.resolve(currentDir, '..', '..', 'contract', 'dist', 'managed', 'tokenless');
   indexer = 'http://127.0.0.1:8088/api/v1/graphql';
   indexerWS = 'ws://127.0.0.1:8088/api/v1/graphql/ws';
   node = 'http://127.0.0.1:9944';
@@ -32,9 +31,9 @@ export class StandaloneConfig implements Config {
 }
 
 export class TestnetRemoteConfig implements Config {
-  privateStateStoreName = QuickStarterPrivateStateId
+  privateStateStoreName = TokenlessPrivateStateId
   logDir = path.resolve(currentDir, '..', 'logs', 'testnet-remote', `${new Date().toISOString()}.log`);
-    zkConfigPath = path.resolve(currentDir, '..', '..', 'contract', 'dist', 'managed', 'quick-starter');
+    zkConfigPath = path.resolve(currentDir, '..', '..', 'contract', 'dist', 'managed', 'tokenless');
   indexer = 'https://indexer-rs.testnet-02.midnight.network/api/v1/graphql';
   indexerWS = 'wss://indexer-rs.testnet-02.midnight.network/api/v1/graphql/ws';
   node = 'https://rpc.testnet-02.midnight.network';
