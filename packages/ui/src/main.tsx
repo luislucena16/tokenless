7c9ec7037c22286bb20a65e25c23d58400aebe3c
luislucena16 <lucenaluis137@gmail.com>
2025-07-21 01:17:15 +0200

    feat: add main.tsx file and polyfills.ts file

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,5 +1,31 @@
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import './polyfills'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import MidnightWalletProvider from "./providers/MidnightWalletProvider.tsx";
+import DeployedContractProvider from "./providers/DeployedContractProvider.tsx";
+import pino from "pino";
+import { setNetworkId, type NetworkId } from "@midnight-ntwrk/midnight-js-network-id";
 
-const App = () => <h1>Hello World!</h1>;
+const networkId = import.meta.env.VITE_NETWORK_ID as NetworkId;
+// Ensure that the network IDs are set within the Midnight libraries.
+setNetworkId(networkId);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// Create a default `pino` logger and configure it with the configured logging level.
+export const logger = pino({
+  level: import.meta.env.VITE_LOGGING_LEVEL as string,
+});
+
+logger.trace('networkId = ', networkId);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MidnightWalletProvider logger={logger}>
+      <DeployedContractProvider logger={logger}>
+        <App />
+      </DeployedContractProvider>
+    </MidnightWalletProvider>
+  </StrictMode>
+);
+
