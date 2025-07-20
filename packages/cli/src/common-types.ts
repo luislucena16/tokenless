1eac27aa9faf915dc2b45935a0af22d13f996f40
luislucena16 <lucenaluis137@gmail.com>
2025-07-21 01:19:54 +0200

    chore: remove all unused files

--- packages/cli/src/common-types.ts
+++
@@ -1,8 +0,0 @@
-  import { MidnightProviders } from "@midnight-ntwrk/midnight-js-types";
-  
-  export const QuickStarterPrivateStateId = "quickStarterPrivateState";
-  export type QuickStarterPrivateStateId = typeof QuickStarterPrivateStateId;
-  export type QuickStarterContractProviders = MidnightProviders<
-    QuickStarterPrivateStateId
-  >;
-  
