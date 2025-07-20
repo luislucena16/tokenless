dd0b632d4008bfe79775aaba24f6154073133258
luislucena16 <lucenaluis137@gmail.com>
2025-07-21 00:41:32 +0200

    feat: add components (App, ProjectBoard, InvestingBoard)

---
+++ packages/ui/src/App.tsx
@@ -0,0 +1,13 @@
+import InvestingBoard from "./components/InvestingBoard";
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+      <InvestingBoard />
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
